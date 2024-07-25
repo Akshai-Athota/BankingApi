b2d32fe077ed43e78bba691234b5dade5548f388
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-07-25 17:25:24 +0530

    pushing intial project set up

---
+++ src/main/java/com/akshai/simple/Banking/Api/SimpleBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.akshai.simple.Banking.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshai/simple/Banking/Api/SimpleBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshai.simple.Banking.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
